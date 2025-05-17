8b632f896a9c479b253a4fe74787f5a53204d3e6
anton240496 <47662779+anton240496@users.noreply.github.com>
2025-05-17 10:01:58 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+      $('.slider').slick({
+        slidesToShow: 1,
+        infinite: false,
+        prevArrow: '<button type="button" class="slick_arrow slick_prev"><img src="img/arrow_prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick_arrow slick_next"><img src="img/arrow_next.svg" alt=""></button>',
+    });
+
+});
