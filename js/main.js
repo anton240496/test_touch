13f5301f124d25541a8440d9a4194fff83d917c3
anton240496 <47662779+anton240496@users.noreply.github.com>
2025-05-17 10:37:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(function(){
       $('.slider').slick({
         slidesToShow: 1,
-        infinite: false,
         prevArrow: '<button type="button" class="slick_arrow slick_prev"><img src="img/arrow_prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick_arrow slick_next"><img src="img/arrow_next.svg" alt=""></button>',
     });
